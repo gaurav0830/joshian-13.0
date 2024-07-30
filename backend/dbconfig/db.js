b64063abe5eadf97b5f121b31ed658e6abc01038
manish-107 <creativecrew2676@gmail.com>
2024-07-30 16:40:59 +0530

    env

--- backend/dbconfig/db.js
+++ backend/dbconfig/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     const MONGODBURLS = process.env.MONGODBURL;
-    await mongoose.connect(MONGODBURLS);
+    await mongoose.connect("mongodb://localhost:27017");
     console.log(`[Database]:Database connected ${MONGODBURLS}`);
   } catch (error) {
     console.log("[Database]: Error while connecting..");
